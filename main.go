fbf88d00ba39d614d28b717fad999887b005a5cd
zjyl1994 <me@zjyl1994.com>
2019-02-17 18:59:38 +0800

    embed web.html in assets.go

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 	"text/template"
 )
 
+var tpl string
+
 func main() {
+	var err error
+	tpl, err = getTemplate()
+	if err != nil {
+		log.Fatalln("Unpack web panel failed", err)
+	}
 	port := flag.String("port", ":58000", "HTTP listen port")
 	flag.Parse()
 	http.HandleFunc("/info", getInfo)
 	http.HandleFunc("/", index)
-	err := http.ListenAndServe(*port, nil)
+	err = http.ListenAndServe(*port, nil)
 	if err != nil {
 		log.Fatalln("ListenAndServe: ", err)
 	}
@@ -29,8 +36,8 @@ func getInfo(w http.ResponseWriter, r *http.Request) {
 func index(w http.ResponseWriter, r *http.Request) {
 	interfaceName := getInterfacesNames()
 	infoString := infoJSON()
-	t := template.Must(template.ParseFiles("./web.html"))
-	t.ExecuteTemplate(w, "web.html", map[string]interface{}{
+	t := template.Must(template.New("tpl").Parse(tpl))
+	t.ExecuteTemplate(w, "tpl", map[string]interface{}{
 		"ifName": interfaceName,
 		"info":   infoString,
 	})
